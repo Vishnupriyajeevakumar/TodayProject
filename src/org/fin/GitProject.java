a703c47329cef297cf1ff697deb142018ce766d0
Vishnupriyajeevakumar <vinu.priya0397@gmail.com>
2021-07-26 10:37:53 +0530

    Upadate Floders

---
+++ src/org/fin/GitProject.java
@@ -0,0 +1,19 @@
+package org.fin;
+
+public class GitProject {
+	
+	private void name() {
+	  System.out.println("Vinu");
+
+	}
+	private void age() {
+	  System.out.println("23");
+
+	}
+	public static void main(String[] args) {
+		GitProject gp = new GitProject();
+		gp.name();
+		gp.age();
+	}
+
+}
