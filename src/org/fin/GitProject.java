a57fc48c287e415d6f3e662ee7872b964573a735
Vishnupriyajeevakumar <vinu.priya0397@gmail.com>
2021-07-26 11:12:56 +0530

    Another Folder Update Two

--- src/org/fin/GitProject.java
+++ src/org/fin/GitProject.java
@@ -10,10 +10,18 @@ public class GitProject {
 	  System.out.println("23");
 
 	}
+	private void salary() {
+		  System.out.println("10 lak");
+	}
+	private void landmark() {
+		  System.out.println("omr");
+	}
 	public static void main(String[] args) {
 		GitProject gp = new GitProject();
 		gp.name();
 		gp.age();
+		gp.salary();
+		gp.landmark();
 	}
 
 }
